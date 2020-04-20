1da00e04e8a53df170fa0ac22bc93af96623dbe6
mika <mirouf83@live.fr>
2020-04-20 17:46:14 +0200

    maj sources minimifiées

--- ressources/Starting website/js/jqBootstrapValidation.js
+++ ressources/Starting website/js/jqBootstrapValidation.js
@@ -189,7 +189,7 @@
               } else if ($this.data("validationEmailMessage")) {
                 message = $this.data("validationEmailMessage");
               }
-              $this.data("validationValidemailMessage", message);			  
+              $this.data("validationValidemailMessage", message);
             }
             // ---------------------------------------------------------
             //                                                MINCHECKED
@@ -478,7 +478,7 @@
                 // How many errors did we find?
                 if (settings.options.semanticallyStrict && errorsFound.length === 1) {
                   // Only one? Being strict? Just output it.
-                  $helpBlock.html(errorsFound[0] + 
+                  $helpBlock.html(errorsFound[0] +
                     ( settings.options.prependExistingHelpBlock ? $helpBlock.data("original-contents") : "" ));
                 } else {
                   // Multiple? Being sloppy? Glue them together into an UL.
